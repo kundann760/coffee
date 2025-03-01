834bde1327c35680acb136516107b777c0dfe7cc
Kundan <mokhalekundan@gmail.com>
2025-03-01 14:15:18 +0530

    resolve build errors 3

--- src/components/ui/moving-border.tsx
+++ src/components/ui/moving-border.tsx
@@ -22,11 +22,12 @@ export function Button({
 }: {
   borderRadius?: string;
   children: React.ReactNode;
-  as?: any;
+  as?: React.ElementType;
   containerClassName?: string;
   borderClassName?: string;
   duration?: number;
   className?: string;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   [key: string]: any;
 }) {
   return (
@@ -80,8 +81,10 @@ export const MovingBorder = ({
   duration?: number;
   rx?: string;
   ry?: string;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   [key: string]: any;
 }) => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const pathRef = useRef<any>(null);
   const progress = useMotionValue<number>(0);
 
